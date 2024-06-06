9f5ab0264aff624f34b6169bbed2386027733423
nimargolis <486016@gmail.com>
2024-06-06 17:24:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,32 +8,38 @@ const util = require('util');
 const kerberos = require('kerberos');
 
 module.exports = async function(plugin) {
-  const { spn } = plugin.params;
+  const { spn='' } = plugin.params;
+  /*
   let kerberosServer = {};
   try {
     kerberosServer = await kerberos.initializeServer(spn);
   } catch (e) {
     plugin.log("Initialization error " + util.inspect(e), 1)
-  }   
+  }
+  */
   
   plugin.onCommand(async message => {
     plugin.log('Get command ' + util.inspect(message), 1);
-    // Response не отправляем. После получения данных отправляем {type:'syncUsers', data:[]}
-
+ 
     if (message.param == 'checkToken') {
       try {
+        const kerberosServer = await kerberos.initializeServer(spn);
+        plugin.log("kerberosServer.step  token=" + message.data, 1);
         await kerberosServer.step(message.data);
+    
+        plugin.log("kerberosServer.username=" + kerberosServer.username, 1);
+        plugin.log("kerberosServer.targetName=" + kerberosServer.targetName, 1);
+        
         message.data = kerberosServer.username;
         plugin.sendResponse(message, 1);
       } catch (e) {
         plugin.sendResponse(message, 0);
         plugin.log("Autorization error " + util.inspect(e), 1)
       }
-    } 
+    } else {
+      plugin.log('Unknown command ' + message.param, 1);
+    }
   });
-
-  
-
 
   /*console.log(kerberosServer.username)
   console.log(kerberosServer.targetName)
